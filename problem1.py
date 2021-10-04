656f8b90af568c481a179109aac60f835d0183da
antun3s <antunes.dll@gmail.com>
2021-10-03 21:18:24 -0300

    commit

---
+++ problem1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def ismultiple(n,m):
+    if n % m == 0:
+        return True
+    else:
+        return False
+
+x = 0 
+for i in range (1000):
+    if ismultiple(i,3) or ismultiple(i,5):
+        x = x +i
+        
+print( 'The sum of all multiple of 3 and 5 between 1 and 100 is {}'.format(x))
+
